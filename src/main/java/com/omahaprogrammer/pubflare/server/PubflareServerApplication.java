d38439b7bffe2205f9c0d4d4d02bf0cfebee06b0
Jonathan Paz <omahaprogrammer@gmail.com>
2020-05-20 12:46:49 -0500

    Initial Project

---
+++ src/main/java/com/omahaprogrammer/pubflare/server/PubflareServerApplication.java
@@ -0,0 +1,13 @@
+package com.omahaprogrammer.pubflare.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubflareServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PubflareServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/omahaprogrammer/pubflare/server/PubflareServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omahaprogrammer.pubflare.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PubflareServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
