eccd2e6f000706c244b79fb2e3b1f16dabc1c059
Jonathan Paz <omahaprogrammer@gmail.com>
2020-05-20 22:50:50 -0500

    Minor format change to test gpg

--- src/main/java/com/omahaprogrammer/pubflare/server/PubflareServerApplication.java
+++ src/main/java/com/omahaprogrammer/pubflare/server/PubflareServerApplication.java
@@ -21,9 +21,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PubflareServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PubflareServerApplication.class, args);
 	}
-
 }
