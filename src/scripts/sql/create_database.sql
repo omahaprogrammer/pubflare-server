7dbb8b3269f938c681a4d63705322737b7e8d87d
Jonathan Paz <omahaprogrammer@gmail.com>
2020-05-25 20:39:21 -0500

    Updated schema

--- src/scripts/sql/create_database.sql
+++ src/scripts/sql/create_database.sql
@@ -14,10 +14,23 @@
 * limitations under the License.
 */
 
+drop table if exists profile_username cascade;
+drop table if exists profile_pictures cascade;
+drop table if exists profile_website cascade;
+drop table if exists profile_email cascade;
+drop table if exists profile_phone cascade;
+drop table if exists profile_address cascade;
+drop table if exists profile_gender cascade;
+drop table if exists profile_birthdate cascade;
 drop table if exists profile cascade;
+
 drop table if exists persistent_logins cascade;
 drop table if exists authorities cascade;
 drop table if exists users cascade;
+drop table if exists acl_entry cascade;
+drop table if exists acl_object_identity cascade;
+drop table if exists acl_class cascade;
+drop table if exists acl_sid cascade;
 
 create table users (
     username text primary key,
@@ -38,31 +51,129 @@ create table persistent_logins (
     last_used timestamp not null
 );
 
+create table acl_sid (
+    id bigserial primary key,
+    principal boolean not null,
+    sid text not null,
+    unique(sid,principal)
+);
+
+create table acl_class (
+    id bigserial primary key,
+    class text unique
+);
+
+create table acl_object_identity (
+    id bigserial primary key,
+    object_id_class bigint not null references acl_class (id),
+    object_id_identity text not null,
+    parent_object bigint references acl_object_identity (id),
+    owner_sid bigint references acl_sid (id),
+    entries_inheriting boolean not null,
+    unique(object_id_class,object_id_identity)
+);
+
+create table acl_entry (
+    id bigserial primary key,
+    acl_object_identity bigint not null references  acl_object_identity (id),
+    ace_order int not null,
+    sid bigint not null references acl_sid (id),
+    mask int not null,
+    granting boolean not null,
+    audit_success boolean not null,
+    audit_failure boolean not null,
+    unique(acl_object_identity,ace_order)
+);
+
 create table profile (
-    profile_id bigserial primary key,
-    user_subject varchar(255) not null,
+    id bigserial primary key,
+    profile_uuid uuid not null,
     full_name text,
     given_name text,
     family_name text,
     middle_name text,
     nickname text,
-    username text unique references users (username),
-    picture bytea,
-    website text,
-    email text,
-    email_verified boolean not null default false,
-    gender text,
-    birthdate date,
     zoneinfo text,
     locale text,
-    phone_number text,
-    phone_number_verified boolean not null default false,
+    created_at timestamp not null default current_timestamp,
+    updated_at timestamp not null default current_timestamp
+);
+
+create table profile_username (
+    id bigserial primary key,
+    profile_id bigint not null references profile (id),
+    username text references users (username),
+    created_at timestamp not null default current_timestamp,
+    updated_at timestamp not null default current_timestamp,
+    unique (profile_id, username)
+);
+
+create table profile_pictures (
+    id bigserial primary key,
+    profile_id bigint not null references profile (id),
+    picture_date bytea not null,
+    main_picture boolean not null default false,
+    created_at timestamp not null default current_timestamp,
+    updated_at timestamp not null default current_timestamp
+);
+
+create table profile_website (
+    id bigserial primary key,
+    profile_id bigint not null references profile (id),
+    website text not null,
+    created_at timestamp not null default current_timestamp,
+    updated_at timestamp not null default current_timestamp
+);
+
+create table profile_email (
+    id bigserial primary key,
+    profile_id bigint not null references profile (id),
+    email_type text,
+    email text not null,
+    email_verified boolean not null default false,
+    main_email boolean not null default false,
+    created_at timestamp not null default current_timestamp,
+    updated_at timestamp not null default current_timestamp
+);
+
+create table profile_phone (
+    id bigserial primary key,
+    profile_id bigint not null references profile (id),
+    phone_type text,
+    phone text not null,
+    phone_verified boolean not null default false,
+    main_phone boolean not null default false,
+    created_at timestamp not null default current_timestamp,
+    updated_at timestamp not null default current_timestamp
+);
+
+create table profile_address (
+    id bigserial primary key,
+    profile_id bigint not null references profile (id),
+    address_type text,
     street_address text,
     locality text,
     region text,
     postal_code text,
     country text,
+    main_address boolean not null default false,
+    created_at timestamp not null default current_timestamp,
+    updated_at timestamp not null default current_timestamp,
+    check (coalesce(street_address, locality, region, postal_code, country) is not null)
+);
+
+create table profile_gender (
+    id bigserial primary key,
+    profile_id bigint not null references profile (id) unique,
+    gender text not null,
     created_at timestamp not null default current_timestamp,
     updated_at timestamp not null default current_timestamp
 );
 
+create table profile_birthdate (
+    id bigserial primary key,
+    profile_id bigint not null references profile (id) unique,
+    birthdate date not null,
+    created_at timestamp not null default current_timestamp,
+    updated_at timestamp not null default current_timestamp
+);
